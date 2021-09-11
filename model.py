123db323f398d9fcb3497a02a371d3b944a486b5
Jaeyoung <71117066+JaeyoungShin2003@users.noreply.github.com>
2021-09-11 22:43:46 +0900

    working on adding lstm model

---
+++ model.py
@@ -0,0 +1,12 @@
+from torch import nn
+import torch
+import numpy as np
+
+class Grid_Net(nn.Module):
+    def __init__(self,input_size, hidden_size):
+        super(Grid_Net,self).__init__()
+        pass
+    # input of velocity, cos and sin of angular velociy, and
+    def forward(self,input,initial_condition):
+        pass
+
