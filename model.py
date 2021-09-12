06cd17dc9ce10e13a646301934205498117d5ab0
Jaeyoung <71117066+JaeyoungShin2003@users.noreply.github.com>
2021-09-12 23:15:49 +0900

    lstm model 80% finished need to work on training script

--- model.py
+++ model.py
@@ -2,11 +2,39 @@ from torch import nn
 import torch
 import numpy as np
 
-class Grid_Net(nn.Module):
-    def __init__(self,input_size, hidden_size):
-        super(Grid_Net,self).__init__()
-        pass
-    # input of velocity, cos and sin of angular velociy, and
-    def forward(self,input,initial_condition):
-        pass
 
+class Grid_Net(nn.Module):
+    def __init__(self, input_size=3, lstm_hidden_layer_size=128, dropout_rate=0.5, bottleneck_size=256,
+                 place_cell_size=256,
+                 head_dir_cell_size=12):
+        super(Grid_Net, self).__init__()
+
+        # place/head dir cell activations -> hidden layer size of lstm
+        self.init_lstm_cell = nn.Linear(place_cell_size + head_dir_cell_size, lstm_hidden_layer_size)
+        self.init_lstm_state = nn.Linear(place_cell_size + head_dir_cell_size, lstm_hidden_layer_size)
+
+        # LSTM for predicting the cell output
+        self.rnn = nn.LSTM(input_size, lstm_hidden_layer_size)
+
+        # bottleneck to cut down activation
+        self.bottleneck = nn.Linear(lstm_hidden_layer_size, bottleneck_size)
+
+        # automatically stops dropout when it is in eval mode
+        self.dropout = nn.Dropout(p=dropout_rate)
+        self.softmax_place_cell = nn.Softmax()
+        self.softmax_head_dir_cell = nn.Softmax()
+        self.place_cell_pred = nn.Linear(bottleneck_size, place_cell_size)
+        self.head_dir_cell_pred = nn.Linear(bottleneck_size, head_dir_cell_size)
+
+    # input of velocity, cos and sin of angular velocity, and hidden state of cell activations
+    def forward(self, inputs, place_cell_acts, head_dir_cell_acts):
+        initial_condition = torch.cat(place_cell_acts, head_dir_cell_acts, dim=1)
+        hidden_state = self.init_lstm_state(initial_condition)
+        cell_state = self.init_lstm_cell(initial_condition)
+        out, (hidden_state_out, cell_state_out) = self.rnn(inputs, (hidden_state, cell_state))
+        out = self.bottleneck(out)
+        out = self.dropout(out)
+        place_cell_prediction = self.softmax_place_cell(self.place_cell_pred(out))
+        head_dir_cell_prediction = self.softmax_head_dir_cell(self.head_dir_cell_pred(out))
+        return place_cell_prediction, head_dir_cell_prediction, hidden_state_out, cell_state_out
+# TODO: implement so that there is layers of stuff that can do backpropagation through time
--- train.py
+++ train.py
@@ -1,4 +1,6 @@
-
+import torch.optim as optim
+import torch
+import torch.nn as nn
 # hyperparameters
 N_EPOCH = 1000
 N_STEPS_PER_EPOCH = 1000
@@ -12,3 +14,9 @@ LEARNING_RATE = 1e-5
 MOMENTUM = 0.9
 BOTTLENECK_DROPOUT_RATE = 0.5
 GRADIENT_CLIPPING = 1e-5
+
+optimizer = optim.RMSprop(None,lr=LEARNING_RATE,alpha=0.9,momentum=MOMENTUM,eps=1e-10)
+def initialization():
+    pass
+def train():
+    pass
